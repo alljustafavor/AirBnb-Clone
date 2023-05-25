dfb216fab30343d3f15dbad5feb7b630ee552d92
Joseph Noblett <noblettjoe@gmail.com>
2023-05-25 00:50:02 -0500

    img fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ import { ReactDOM } from "react";
 export default function Navbar() {
     return (
         <nav>
-            <img src="./src/assets/airbnb-logo.png" className="nav--logo"/>
+            <img src="//src/assets/airbnb-logo.png" className="nav--logo"/>
         </nav>
     )
 }
